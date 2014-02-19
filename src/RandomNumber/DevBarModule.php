11959377cc343b41607a228502f629739785b641
yonni <yonni.m@zend.com>
2014-02-19 15:08:16 +0200

    Extracted all events management functionality out to abstract class

--- src/RandomNumber/DevBarModule.php
+++ src/RandomNumber/DevBarModule.php
@@ -1,42 +1,15 @@
 <?php
 namespace RandomNumber;
 
-use Zend\EventManager\ListenerAggregateInterface;
-use Zend\EventManager\EventManagerInterface;
 use Zend\View\Model\ViewModel;
+use DevBar\Listener\AbstractDevBarModule;
 
-class DevBarModule implements ListenerAggregateInterface
+class DevBarModule extends AbstractDevBarModule
 {
-    /**
-     * @var \Zend\Stdlib\CallbackHandler[]
-     */
-    protected $listeners = array();
-
-    /* (non-PHPdoc)
-     * @see \Zend\EventManager\ListenerAggregateInterface::attach()
-     */
-    public function attach(EventManagerInterface $events)
-    {
-        $sharedEvents      = $events->getSharedManager();
-        $this->listeners[] = $sharedEvents->attach('devbar', 'DevBarModules', array($this, 'produceViewModel'), 100);
-    }
-
-    /* (non-PHPdoc)
-     * @see \Zend\EventManager\ListenerAggregateInterface::detach()
-     */
-    public function detach(EventManagerInterface $events)
-    {
-        foreach ($this->listeners as $index => $listener) {
-            if ($events->detach($listener)) {
-                unset($this->listeners[$index]);
-            }
-        }
-    }
-    
     /**
      * @return \Zend\View\Model\ViewModel
      */
-    public function produceViewModel() {
+    public function __invoke() {
         $randomNumber = new RandomNumber();
         $viewModel = new ViewModel(array('randomnumber' => $randomNumber->random()));
         $viewModel->setTemplate('random/randomnumber');
